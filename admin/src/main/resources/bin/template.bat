61c54bc4fc101a377eff86bfcad5aa57b7cc2966
bcarlsenca <bcarlsen@westcoastinformatics.com>
2014-10-14 16:07:29 -0700

    TASK 10- more archetypes work.

--- admin/src/main/resources/bin/template.bat
+++ admin/src/main/resources/bin/template.bat
@@ -34,7 +34,7 @@ set distReposSnapUrl=http://va-archiva-host:8082/archiva/repository/va-snapshots
 set dbGroupId=gov.va.isaac.db
 set dbArtifactId=solor-all
 set dbVersion=2014.10.13
-set dbClassifier=bdb
+set dbClassifier=active-only
 
 :: Users version info - PLACEHOLDER
 :: set usersGroupId=gov.va.isaac.users
